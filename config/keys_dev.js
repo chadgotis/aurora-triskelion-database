40273495c8227eb8a9f9d8f6ac63ae0528a5e83a
Chadric Gotis <30946867+chadgotis@users.noreply.github.com>
2020-12-10 19:05:24 +0800

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://chadgotis:gotis1997@dev-aurora.c1jld.mongodb.net/dev-aurora?retryWrites=true&w=majority",
-  secretOrKey: "joker-december-18-2016",
-};
